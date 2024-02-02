9ee4c69ef0eeed2aa76ee7967d527e54e4e33ac0
Jose Torres <vidojam@gmail.com>
2024-02-02 13:28:11 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+
+    });
+    $( "#loginButton" ).click(function() {
+         $("#reserveModal").modal("show");
+    });
+   
+    });
+    $( "#loginButton" ).click(function() {
+        $("#reserveModal").modal("show");
+    });
+
+
