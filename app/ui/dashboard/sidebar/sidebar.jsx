f0f046d76047506445850f3ab80bf93fff25e413
Jose Torres <vidojam@gmail.com>
2024-02-03 14:36:40 -0500

    fixed jsconfig bug

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,9 +1,12 @@
 import styles from "./sidebar.module.css"
+import {MdDashboard,MdSupervisedUserCircle,MdShoppingBag,MdAttachMoney,MdWork,MdAnalytics,MdPeople,MdOutlineSettings,MdHelpCenter,MdLogout    }from "react-icons/md"
 
 const Sidebar = () => {
   return (
     <div className={styles.container}>Sidebar</div>
   )
 }
+
+
 
 export default Sidebar
