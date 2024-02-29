86bfce2e2545f06e5697559e746c557ef8fb7518
Jose Torres <vidojam@gmail.com>
2024-02-29 10:31:11 -0500

    working on namrbar

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,12 +1,15 @@
 import { usePathname } from 'next/navigation';
 import styles from './navbar.module.css'
+import {MdNotifications, MdOutlineChat, MdPublic, MdSearch } from "react-icons/md;"
 
 const Navbar = () => {
 
   const pathname=usePathname();
-
+   
   return (
-    <div className={StyleSheet.container}>Navbar</div>
+    <div className={StyleSheet.container}>
+      <div className={styles.title}>{pathname}</div>
+    </div>
   )
 }
 
