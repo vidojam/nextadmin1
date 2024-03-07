9e0cbba6a0c3191015f82de9858485ff31bd9557
Jose Torres <vidojam@gmail.com>
2024-03-07 10:27:41 -0500

    work on navbar

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -12,7 +12,12 @@ const Navbar = () => {
       <div className={styles.menu} >
         <div className={styles.search}>
           <MdSearch/>
-          <input type="text" placeholder="Search..." />
+          <input type="text" placeholder="Search..." className={"styles.input"} />
+        </div>
+        <div className={styles.icons}>
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
         </div>
       </div> 
       </div>
